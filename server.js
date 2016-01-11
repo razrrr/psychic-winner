cd2ec073819784337890b34a0ffc1d05da057d20
Eric <eric.chen.again@gmail.com>
2016-01-11 02:05:10 -0500

    Minor tweaks
    updated to angular 1.4.2 from 1.3.15
    remove path.join from expess.static

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ var gameState = {
 // start the server
 var express = require("express");
 var path = require("path");
-var app = express();
-app.use(express.static(path.join(__dirname, "public")));
-var server = require("http").createServer(app).listen(process.env.PORT || 8081);
+var server = require("http").createServer(express.static("public")).listen(process.env.PORT || 8081);
 var io = require("socket.io").listen(server);
 
 // listen for connections
