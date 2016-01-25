razrrr/psychic-winner

commit 448e9a297d63379479864a35d9999bc02c8addb4

Add function to send individual gameStates to each client
obscure other player’s data from each client

server.js
@@ -21,13 +21,12 @@ var gameState = {
     playerOrder: [],
 };
 
-function Player(id, deck) {
+function Player(id) {
     this.id = id;
-    this.connected = true;
     this.hand = [];
     this.discarded = [];
     this.played = [];
-    this.deck = deck;
+    this.deck = [];
     this.coins = 0;
     this.actions = 1;
     this.buys = 1;
@@ -51,22 +50,42 @@ io.sockets.on("connection", function(socket) {
     if (gameState.phase === "pregame") {
         gameState.players[socket.id] = new Player(socket.id);
         gameState.playerOrder.push(socket.id);
-    } else {
-        for (var id in gameState.players) {
-            if (gameState.players[id].connected === false) {
-                if (gameState.playerOrder[gameState.activePlayer] === gameState.players[id]) gameState.playerOrder[gameState.activePlayer] = socket.id;
-                for (var i in gameState.playerOrder) {
-                    if (gameState.playerOrder[i] === id) gameState.playerOrder[i] = socket.id;
-                }
-                gameState.players[socket.id] = gameState.players[id];
-                gameState.players[socket.id].connected = true;
-                gameState.players[socket.id].id = socket.id;
-                delete gameState.players[id];
-                break;
-            }
-        }
     }
-    io.sockets.emit("gameState", gameState);
+    sendGameStates();
+
+    // ===============
+    // Define client message responses
+    // ===============
+    // ----------------
+    // Received startGame message from client. User started the game
+    // ----------------
+    socket.on("startGame", function() {
+        gameStart = true;
+        io.sockets.emit("log", "game started");
+
+        gameState.trash = [];
+
+        for (var id in gameState.players) {
+            gameState.players[id].deck = createStartingHand();
+            shuffle(gameState.players[id].deck);
+            draw(gameState.players[id], 5);
+        }
+
+        // initialize board - select 10 random action cards
+        gameState.board = initBoard();
+
+        // !! <DEBUG> Put all cards into play. Delete this section later.
+        // gameState.board = [];
+        // for (var key in cards) {
+        //     var newCard = createCard(key);
+        //     newCard.supply = 10;
+        //     gameState.board.push(newCard);
+        //     cards[key].bankVersion = newCard;
+        // }
+
+        gameState.phase = "action";
+        sendGameStates();
+    });
     // ----------------
     // Received endTurn message from client. User ended their turn
     // ----------------
@@ -81,7 +100,7 @@ io.sockets.on("connection", function(socket) {
             gameState.activePlayer = (gameState.activePlayer + 1) % gameState.playerOrder.length;
         }
 
-        io.sockets.emit("gameState", gameState);
+        sendGameStates();
     });
     // ----------------
     // Received buy message from client. User bought a card from the bank
@@ -109,7 +128,7 @@ io.sockets.on("connection", function(socket) {
                 }
             }
 
-            io.sockets.emit("gameState", gameState);
+            sendGameStates();
         }
     });
     // ----------------
@@ -125,7 +144,7 @@ io.sockets.on("connection", function(socket) {
             player.played.push(player.hand[data.cardIndex]);
             player.hand.splice(data.cardIndex, 1);
             card.action(player);
-            io.sockets.emit("gameState", gameState);
+            sendGameStates();
         }
         if (card.type.indexOf("treasure") >= 0) {
             io.sockets.emit("log", player.id + " plays " + card.name);
@@ -133,39 +152,8 @@ io.sockets.on("connection", function(socket) {
             player.played.push(player.hand[data.cardIndex]);
             player.hand.splice(data.cardIndex, 1);
             card.action(player);
-            io.sockets.emit("gameState", gameState);
+            sendGameStates();
         }
-    });
-    // ----------------
-    // Received startGame message from client. User started the game
-    // ----------------
-    socket.on("startGame", function() {
-        gameStart = true;
-        io.sockets.emit("log", "game started");
-
-        gameState.trash = [];
-
-        for (var id in gameState.players) {
-            gameState.players[id].deck = createStartingHand();
-            shuffle(gameState.players[id].deck);
-            draw(gameState.players[id], 5);
-        }
-
-        // initialize board
-        // select 10 random action cards
-        gameState.board = initBoard();
-
-        // !! <DEBUG> Put all cards into play. Delete this section later.
-        gameState.board = [];
-        for (var key in cards) {
-            var newCard = createCard(key);
-            newCard.supply = 10;
-            gameState.board.push(newCard);
-            cards[key].bankVersion = newCard;
-        }
-        gameState.phase = "action";
-        io.sockets.emit("gameStart");
-        io.sockets.emit("gameState", gameState);
     });
     // ----------------
     // Received select message from client. User made an option from the "select" menu
@@ -186,14 +174,10 @@ io.sockets.on("connection", function(socket) {
     // ----------------
     socket.on("disconnect", function() {
         io.sockets.emit("log", socket.id + " disconnected");
-        if (gameState.phase != "pregame") {
-            gameState.players[socket.id].connected = false;
-            io.sockets.emit("gameState", gameState);
-        } else {
-            delete gameState.players[socket.id];
-            gameState.playerOrder.splice(gameState.playerOrder.indexOf(socket.id), 1);
-            io.sockets.emit("gameState", gameState);
-        }
+        if (gameState.phase != "pregame") return;
+        delete gameState.players[socket.id];
+        gameState.playerOrder.splice(gameState.playerOrder.indexOf(socket.id), 1);
+        sendGameStates();
     });
 });
 
@@ -236,10 +220,10 @@ function endTurn(player) {
     player.buys = 111;
     player.coins = 110;
     clear(player);
-    io.sockets.emit("gameState", gameState);
+    sendGameStates();
     draw(player, 5);
     gameState.phase = "action";
-    io.sockets.emit("log", gameState.players[gameState.playerOrder[(gameState.activePlayer + 1) % gameState.playerOrder.length]].id + "'s turn");
+    io.sockets.emit("log", gameState.players[gameState.playerOrder[gameState.activePlayer]].id + "'s turn");
 }
 
 // initialize game board
@@ -522,9 +506,36 @@ function countVictoryPoints() {
     }
 }
 
-// sends clients updated gamestates. filters out data that other clients should not see
-function sendGameState() {
-    // note. make copies of gamestates
+// send clients updated gameStates
+function sendGameStates() {
+    for (var id in gameState.players) {
+        var pGameState = createPlayerGameState(id);
+        io.sockets.connected[id].emit("gameState", pGameState);
+    }
+}
 
-    io.sockets.emit("gameState", gameState);
+// create copies of gameStates to information about other player's cards
+function createPlayerGameState(player) {
+    var pGameState = JSON.parse(JSON.stringify(gameState));
+    for (var id in pGameState.players) {
+        var this_player = pGameState.players[id];
+
+        // hide other players information before sending to client
+        if (player != this_player.id) {
+            // hide hand
+            for (var c in this_player.hand) {
+                this_player.hand[c].id = '';
+            }
+            // hide deck
+            for (var c in this_player.deck) {
+                this_player.deck[c].id = '';
+            }
+            // hide discard
+            for (var c in this_player.discarded) {
+                this_player.discarded[c].id = '';
+            }
+        }
+    }
+
+    return pGameState;
 }

public/cards.js
@@ -135,14 +135,14 @@ cards = {
                             callback: function(data) {
                                 player.hand.push(data[0].card);
                                 gameState.phase = "action";
-                                io.sockets.emit("gameState", gameState);
+                                sendGameStates();
                             }
                         };
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                     if (data.length === 0) {
                         gameState.phase = "action";
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                 }
             };
@@ -187,7 +187,7 @@ cards = {
                     io.sockets.emit("log", " ... and gets " + cards[acquiredCard.id].name);
                     player.discarded.push(acquiredCard);
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             };
         }
@@ -225,7 +225,7 @@ cards = {
                         player.hand.splice(cardIndex, 1);
                     }
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             };
         }
@@ -257,7 +257,7 @@ cards = {
                         player.coins += 3;
                     }
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             };
         }
@@ -411,7 +411,7 @@ cards = {
                         draw(player, 1);
                         io.sockets.emit("log", " ... and gets +1 Card");
                     }
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             };
         }
@@ -442,7 +442,7 @@ cards = {
                         }
                     };
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             };
         }
@@ -481,7 +481,7 @@ cards = {
                             io.sockets.emit("log", " gains +1 Coin.");
                         }
                         gameState.phase = "action";
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                 }
             };
@@ -537,11 +537,11 @@ cards = {
                                         player.hand.splice(cardIndex, 1);
                                     }
                                     gameState.phase = "action";
-                                    io.sockets.emit("gameState", gameState);
+                                    sendGameStates();
                                 }
                             }
                         }
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                 }
             };
@@ -587,14 +587,14 @@ cards = {
                                 io.sockets.emit("log", " ... and gets " + data[0].card.name);
                                 player.discarded.push(data[0].card);
                                 gameState.phase = "action";
-                                io.sockets.emit("gameState", gameState);
+                                sendGameStates();
                             }
                         };
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                     if (data.length === 0) {
                         gameState.phase = "action";
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                 }
             };
@@ -626,7 +626,7 @@ cards = {
                         player.coins += 2;
                     };
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             };
         }
@@ -672,14 +672,14 @@ cards = {
                                     player.discarded.push(data[0].card);
                                 }
                                 gameState.phase = "action";
-                                io.sockets.emit("gameState", gameState);
+                                sendGameStates();
                             }
                         };
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                     if (data.length === 0) {
                         gameState.phase = "action";
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                 }
             };
@@ -711,7 +711,7 @@ cards = {
                             io.sockets.emit("log", " gains +2 Actions.");
                         }
                         gameState.phase = "action";
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                 }
             };
@@ -822,10 +822,10 @@ cards = {
                         if (gameState.activePlayer === currentPlayer) {
                             player.coins += 2;
                             gameState.phase = "action";
-                            io.sockets.emit("gameState", gameState);
+                            sendGameStates();
                         } else {
                             attack();
-                            io.sockets.emit("gameState", gameState);
+                            sendGameStates();
                         }
                     }
                 };
@@ -871,11 +871,11 @@ cards = {
                                         player.discarded.push(gameState.revealed.pop());
                                     }
                                     gameState.phase = "action";
-                                    io.sockets.emit("gameState", gameState);
+                                    sendGameStates();
                                 }
                             }
                         };
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     } else {
                         gameState.revealed.push(revealedCard);
                         io.sockets.emit("log", " ... reveals " + cards[revealedCard.id].name);
@@ -927,7 +927,7 @@ cards = {
                     draw(player, data.length);
                     player.actions += 1;
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             }
         }
@@ -986,11 +986,11 @@ cards = {
                                 }
                                 gameState.revealed = [];
                                 gameState.phase = "action";
-                                io.sockets.emit("gameState", gameState);
+                                sendGameStates();
                             }
                         }
                     } else gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             }
             if (victoryCount > 0) gameState.queryData.message = "Put revealed Victory cards into hand.";
@@ -1019,7 +1019,7 @@ cards = {
                     player.hand.splice(data[0].index, 1);
                     io.sockets.emit("log", "... and puts 1 card from hand on top of deck.");
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             }
         }
@@ -1069,13 +1069,13 @@ cards = {
                                 player.coins += 4;
                                 io.sockets.emit("log", " discards an Estate and gains +4 Coins.")
                                 gameState.phase = "action";
-                                io.sockets.emit("gameState", gameState);
+                                sendGameStates();
                             } else if (choiceIndexArray[0] === 1) {
                                 var acquiredCard = acquire(player, "estate");
                                 player.discarded.push(acquiredCard);
                                 io.sockets.emit("log", " gains an Estate.");
                                 gameState.phase = "action";
-                                io.sockets.emit("gameState", gameState);
+                                sendGameStates();
                             }
                         }
                     }
@@ -1086,7 +1086,7 @@ cards = {
                 player.discarded.push(acquiredCard);
                 io.sockets.emit("log", " gains an Estate.");
                 gameState.phase = "action";
-                io.sockets.emit("gameState", gameState);
+                sendGameStates();
             }
         }
     },
@@ -1136,10 +1136,10 @@ cards = {
                                 io.sockets.emit("log", " you named incorrectly.");
                             }
                             gameState.phase = "action";
-                            io.sockets.emit("gameState", gameState);
+                            sendGameStates();
                         }
                     }
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             }
         }
@@ -1209,7 +1209,7 @@ cards = {
                                 libDraw();
                             }
                         }
-                        io.sockets.emit("gameState", gameState);
+                        sendGameStates();
                     }
                     else libDraw();
                 }
@@ -1219,7 +1219,7 @@ cards = {
                         player.discarded.push(gameState.revealed.pop(i));
                     }
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             }
             libDraw();
@@ -1273,10 +1273,10 @@ cards = {
                                 i--;
                             }
                             gameState.phase = "action";
-                            io.sockets.emit("gameState", gameState);
+                            sendGameStates();
                         }
                     }
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             }
         }
@@ -1304,7 +1304,7 @@ cards = {
                     player.discarded.push(acquiredCard);
                     gameState.trash.push(player.played.pop());
                     gameState.phase = "action";
-                    io.sockets.emit("gameState", gameState);
+                    sendGameStates();
                 }
             };
         }

commit 4e8a685d8dafad4e595455e0f126a7dbfe6c4469

Restored reconnection feature
Feature broken after 448e9a2
io.sockets.connected is not a substitute for
gameState.players[id]connected for tracking connected players.
gameState.players[id].connected is set to false when a player
disconnects because we want to let the server know that we have player
data without a player. when the player reconnects or a substitute player
connects the new socket id is assigned to any available disconnected
player data allowing the new connection to take over for the previously
abandoned player data.

server.js
@@ -23,6 +23,7 @@ var gameState = {
 
 function Player(id) {
     this.id = id;
+    this.connected = true;
     this.hand = [];
     this.discarded = [];
     this.played = [];
@@ -50,6 +51,20 @@ io.sockets.on("connection", function(socket) {
     if (gameState.phase === "pregame") {
         gameState.players[socket.id] = new Player(socket.id);
         gameState.playerOrder.push(socket.id);
+    } else {
+        for (var id in gameState.players) {
+            if (gameState.players[id].connected === false) {
+                if (gameState.playerOrder[gameState.activePlayer] === gameState.players[id]) gameState.playerOrder[gameState.activePlayer] = socket.id;
+                for (var i in gameState.playerOrder) {
+                    if (gameState.playerOrder[i] === id) gameState.playerOrder[i] = socket.id;
+                }
+                gameState.players[socket.id] = gameState.players[id];
+                gameState.players[socket.id].connected = true;
+                gameState.players[socket.id].id = socket.id;
+                delete gameState.players[id];
+                break;
+            }
+        }
     }
     sendGameStates();
 
@@ -174,9 +189,12 @@ io.sockets.on("connection", function(socket) {
     // ----------------
     socket.on("disconnect", function() {
         io.sockets.emit("log", socket.id + " disconnected");
-        if (gameState.phase != "pregame") return;
-        delete gameState.players[socket.id];
-        gameState.playerOrder.splice(gameState.playerOrder.indexOf(socket.id), 1);
+        if (gameState.phase != "pregame") {
+            gameState.players[socket.id].connected = false;
+        } else {
+            delete gameState.players[socket.id];
+            gameState.playerOrder.splice(gameState.playerOrder.indexOf(socket.id), 1);
+        }
         sendGameStates();
     });
 });
@@ -509,8 +527,10 @@ function countVictoryPoints() {
 // send clients updated gameStates
 function sendGameStates() {
     for (var id in gameState.players) {
-        var pGameState = createPlayerGameState(id);
-        io.sockets.connected[id].emit("gameState", pGameState);
+        if (gameState.players[id].connected) {
+            var pGameState = createPlayerGameState(id);
+            io.sockets.connected[id].emit("gameState", pGameState);
+        }
     }
 }
 