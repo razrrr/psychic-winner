33a47cb5c136edca342fcb90e415e08c4d9c61a5
THlEF <EkliP5E@Kevins-MacBook-Pro.local>
2016-02-03 22:59:49 -0500

    asdf
    asdf

--- public/cards.js
+++ public/cards.js
@@ -1642,4 +1642,89 @@ cards = {
 
         }
     },
+    "bureaucrat": {
+        expansion: "Base",
+        description: "Gain a silver card; put it on top of your deck. Each other player reveals a Victory card from his hand and puts it on his deck (or reveals a hand with no Victory cards).",
+        name: "Torturer",
+        type: "action attack",
+        cost: 5,
+        value: 0,
+        victory: function(player) {
+            return 0;
+        },
+        action: function(player) {
+            // save current player id so we know when all players have been attacked
+            var currentPlayer = gameState.activePlayer;
+            gameState.activePlayer = (gameState.activePlayer + 1) % gameState.playerOrder.length;
+
+            
+            var attack = function() {
+                var playerData = gameState.players[gameState.playerOrder[gameState.activePlayer]];
+                var twoCounter = function () {
+                    if (playerData.hand.length < 2) return playerData.hand.length;
+                    else return 2;
+                }
+                gameState.queryData = {
+                    number: 1,
+                    exact: true,
+                    message: "Choose one.",
+                    choices: ["Discard 2 Cards", "Acquire curse to hand"],
+                    selected: [],
+                    callback: function(choiceIndexArray) {
+                        if (choiceIndexArray[0] === 0) {
+                            gameState.phase = "select";
+                            gameState.queryData = {
+                                eligible: ".you .player .hand .card",
+                                number: twoCounter(),
+                                unique: true,
+                                exact: true,
+                                message: "Select cards to discard",
+                                selected: [],
+                                callback: function(data) {
+                                    var targetCardIndices = [];
+                                    for (var i in data) {
+                                        targetCardIndices.push(data[i].index);
+                                    }
+                                    targetCardIndices.sort(function(a, b) {
+                                        return b - a;
+                                    });
+                                    for (var i = 0; i < targetCardIndices.length; i++) {
+                                        var cardIndex = targetCardIndices[i];
+                                        playerData.discarded.push(playerData.hand[cardIndex]);
+                                        io.sockets.emit("log", " ... " + playerData.id + " discards " + cards[playerData.hand[cardIndex].id].name);
+                                        playerData.hand.splice(cardIndex, 1);
+                                    }
+                                    gameState.activePlayer = (gameState.activePlayer + 1) % gameState.playerOrder.length;
+                                    if (gameState.activePlayer === currentPlayer) {
+                                        draw(player, 3);
+                                        gameState.phase = "action";
+                                        sendGameStates();
+                                    } else {
+                                        gameState.phase = "choose";
+                                        attack();
+                                        sendGameStates();
+                                   }
+                                }
+                            }
+                        }
+                        else if (choiceIndexArray[0] === 1) {
+                            var acquiredCard = acquire(playerData, "curse");
+                            playerData.hand.push(acquiredCard);
+                            gameState.activePlayer = (gameState.activePlayer + 1) % gameState.playerOrder.length;
+                            if (gameState.activePlayer === currentPlayer) {
+                                draw(player, 3);
+                                gameState.phase = "action";
+                                sendGameStates();
+                            } else {
+                                attack();
+                                sendGameStates();
+                            }
+                        }
+                        sendGameStates();
+                    }
+                };
+            }
+            attack();
+        }
+    },
 };
